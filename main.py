c74b48b3f23e7cba9fdd072c6a89b7324b43700c
Artem7sk <92425901+Artem7sk@users.noreply.github.com>
2025-07-02 14:13:33 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-from telegram.ext import Application, CommandHandler
+import os
+from dotenv import load_dotenv
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
 
-def start(update, context):
-    update.message.reply_text("Привет! Я MasloPro бот.")
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Привет! Я MasloPro бот.")
 
 def main():
-    token = "7761025348:AAHqSDrT5zjFY2r-qTOtYazJlLICK4jlKGQ"  # ВСТАВЬ СВОЙ ТОКЕН СЮДА
-
-    app = Application.builder().token(token).build()
+    app = Application.builder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.run_polling()
 
